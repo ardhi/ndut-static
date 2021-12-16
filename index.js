50783cd51124cca5290033ccd1d8d7b90196c221
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-16 19:28:42 +0700

    Refactor & version bump

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
-module.exports = async function (fastify) {
-  const { fp, fs, aneka, getNdutConfig } = fastify.ndut.helper
+module.exports = async function () {
+  const { fp, fs, aneka, getNdutConfig } = this.ndut.helper
   const { pathResolve } = aneka
-  const { config } = fastify
+  const { config } = this
   const name = 'ndut-static'
-  const options = getNdutConfig(fastify, 'ndut-static') || {}
+  const options = getNdutConfig('ndut-static') || {}
   options.root = options.root || (config.dir.base + '/static')
   if (!path.isAbsolute(options.root)) options.root = pathResolve(options.root)
   fs.ensureDirSync(options.root)
