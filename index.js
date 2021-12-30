6d4e35f12049029d5f3619cdb87695fbac479e7a
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-30 07:30:13 +0700

    Bug fix

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const path = require('path')
 
 module.exports = async function () {
-  const { fp, fs, aneka, getNdutConfig } = this.ndut.helper
-  const { pathResolve } = aneka
-  const { config } = this
+  const { fp, getNdutConfig } = this.ndut.helper
   const name = 'ndut-static'
   const options = getNdutConfig('ndut-static') || {}
-  options.root = options.root || (config.dir.base + '/static')
-  if (!path.isAbsolute(options.root)) options.root = pathResolve(options.root)
   const earlyPlugin = fp(require('./lib/early-plugin'))
 
   return { name, options, earlyPlugin }
